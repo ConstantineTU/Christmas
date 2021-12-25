64b20795d258661c5426865d0072b2931800d3e4
ConstantineTU <8kozmo8@gmail.com>
2021-12-25 15:02:55 +0500

    feat: add changes to self-test

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,6 @@ import App from './components/app/app';
 import './assets/favicon.png';
 
 import './assets/scss/style.scss';
-console.group('%cCross-check: Christmas-task Part 1, ConstantineTU', 'color: red');
-console.log('%cНе выполненные пункты: Все пункты выполнены', 'color: red');
-console.log(
-  `Score 200 / 200
-
-	Выполненные пункты: Все пункты выполены.
-Своя собственная фича - добавлена кнопка быстрого возврата к фильтрам, что повышает удобство пользования приложением, а так-же считаю, что я сделал качественно оформленное приложение, визуально красивое, я очень много времени потратил на вёрстку, помимо этого реализован хороший адаптив под телефоны вплоть до 320px.
-`
-);
-console.log('	%cИтого 210 баллов из 200', 'color: green');
-
-console.log('%cМой дискорд - https://discordapp.com/users/414360051101466624', 'color: blue');
-console.log('%cСпасибо за проверку, с Наступающим Вас новым годом и успехов!', 'color: green');
-
-console.groupEnd();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -29,3 +14,21 @@ ReactDOM.render(
 
 // const app = new App();
 // app.start();
+const selfTest = () => {
+  console.group('%cCross-check: Christmas-task Part 1, ConstantineTU', 'color: red');
+  console.log('%cНе выполненные пункты: Все пункты выполнены', 'color: red');
+  console.log(
+    `Score 200 / 200
+
+	Выполненные пункты: Все пункты выполены.
+Своя собственная фича - добавлена кнопка быстрого возврата к фильтрам, что повышает удобство пользования приложением, а так-же считаю, что я сделал качественно оформленное приложение, визуально красивое, я очень много времени потратил на вёрстку, помимо этого реализован хороший адаптив под телефоны вплоть до 320px.
+`
+  );
+  console.log('	%cИтого 210 баллов из 200', 'color: green');
+
+  console.log('%cМой дискорд - https://discordapp.com/users/414360051101466624', 'color: blue');
+  console.log('%cСпасибо за проверку, с Наступающим Вас новым годом и успехов!', 'color: green');
+
+  console.groupEnd();
+}
+selfTest()
